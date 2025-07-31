ef0fc03be023318ad3f06844931d92c028ae8383
Oshinpou <oshinpounikar@gmail.com>
2025-07-31 17:08:43 +0530

    Create products-data.js

---
+++ products-data.js
@@ -0,0 +1,35 @@
+const allProducts = [
+  {
+    name: "Soleme Cheese",
+    price: 1,
+    category: "food",
+    subcategory: "breakfast",
+    image: "soleme-cheese.jpg",
+    link: "product-soleme-cheese.html"
+  },
+  {
+    name: "Iconic Blue Chocolate",
+    price: 499,
+    category: "food",
+    subcategory: "chocolate",
+    image: "iconic-blue-chocolate.jpg",
+    link: "product-iconic-blue-chocolate.html"
+  },
+  {
+    name: "Grow Magic Jam",
+    price: 249,
+    category: "food",
+    subcategory: "breakfast",
+    image: "magic-jam.jpg",
+    link: "product-magic-jam.html"
+  },
+  {
+    name: "Need Crunchy Nuts",
+    price: 379,
+    category: "food",
+    subcategory: "snacks",
+    image: "crunchy-nuts.jpg",
+    link: "product-crunchy-nuts.html"
+  }
+  // Later more products from other pages can be added here
+];
