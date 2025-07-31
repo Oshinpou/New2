21f92d22acbd8da1a321e98b57bce199fb44ff1a
Oshinpou <oshinpounikar@gmail.com>
2025-07-31 17:43:26 +0530

    Create pages-data.js

---
+++ pages-data.js
@@ -0,0 +1,10 @@
+// pages-data.js
+const pagesData = {
+  // Example:
+  // "imacx-softskin": {
+  //   title: "Imacx Soft Skin Serum",
+  //   description: "Achieve gleaming skin with power-packed peptides.",
+  //   price: 2200,
+  //   image: "softskin.jpg"
+  // }
+};
